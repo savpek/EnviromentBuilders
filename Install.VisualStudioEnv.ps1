90c6e6767578c7fe86851e55ce27ab6d8eb7d0e4
Pekka Savolainen <savolainen.pekka@gmail.com>
2013-11-02 10:19:03 +0200

    Updated to visual studio 2013.

--- Install.VisualStudioEnv.ps1
+++ Install.VisualStudioEnv.ps1
@@ -2,7 +2,7 @@ function Installer($scriptPath) {
     . "$scriptPath\InstallFunctions\CommonFunctions.ps1"
     . "$scriptPath\InstallFunctions\CommonPaths.ps1"
 
-    Install VisualStudio2012Premium
+    Install VisualStudio2013Professional
     Install resharper
     Install "Dogtail.VS2012.3"
     Install "python"
