62ae936e90dbc4e0b3a8c0e9bbc714112aabd661
Pekka Savolainen <savolainen.pekka@gmail.com>
2015-05-13 21:45:33 +0300

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -17,7 +17,7 @@ InstallChocolatey
 $env:Path += ";C:\programdata\chocolatey\bin"
 choco feature enable -n allowGlobalConfirmation 
 
-Install "git" "git.exe"
+Install "git.commandline" "git.exe"
 Install "notepad2"
 Install "winmerge" "WinMergeU.exe"
 Install "7Zip"
